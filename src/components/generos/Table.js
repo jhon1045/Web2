e7120acc4d086e97a580edbf6cc13267cfaa8c84
jhon mauro <jhmazaja14@gmail.com>
2024-04-20 23:13:13 -0500

    actualizado

---
+++ src/components/generos/Table.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import TrGenero from './TrGenero'
+
+
+export default function Table({generos = []}) {
+  return (
+    <table className="table">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Nombre</th>
+          <th scope="col">Descripc.</th>
+          <th scope="col">Creado</th>
+          <th scope="col">Estado</th>
+          <th>Acciones</th>
+        </tr>
+      </thead>
+      <tbody>
+        {
+          generos.map((genero, index) => {
+            return  (
+              <TrGenero genero={genero} key={index+1} index={index}/>
+          )
+          })
+        }
+
+      </tbody>
+    </table>
+  )
+}
---
+++ src/components/generos/TrGenero.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import dayjs from 'dayjs'
+
+export default function TrGenero({index, genero}) {
+  return (
+    <tr >
+        <th scope="row">{index+1}</th>
+        <td>{genero.nombre}</td>
+        <td>{genero.descripcion}</td>
+        <td>{dayjs(genero.fechaCreacion).format('YYYY-MM-DD')}</td>
+        <td>
+        {genero.estado ? 'Activo' : 'Inactivo'}
+        </td>
+        <td>
+            <button 
+                type="button" 
+                className="btn btn-outline-success"
+            >
+                Editar
+            </button>
+            <button 
+                type="button" 
+                className="btn btn-outline-danger"
+            >
+                Borrar
+            </button>
+        </td>
+    </tr>
+  )
+}
